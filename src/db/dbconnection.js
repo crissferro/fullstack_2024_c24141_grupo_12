ff9da6acc3b752dd534cf32a19955fc1a018e686
crissferro <crissferrogmail.com>
2024-06-24 23:49:27 -0300

    modifico db al sitio alwaysdata

--- src/db/dbconnection.js
+++ src/db/dbconnection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2')
 
 const pool = mysql.createPool({
-	host: 'tay.h.filess.io',
-	user: 'solocaps_highwayhot',
-	password: 'e85edbcfa15ffa946f90e243812a388156b02b0f',
-	database: 'solocaps_highwayhot',
-	port: 3307,
+	host: 'mysql-solocaps.alwaysdata.net',
+	user: 'solocaps',
+	password: 'Qwerty1234Admin',
+	database: 'solocaps_db',
+	port: 3306,
 	waitForConnections: true,
 	connectionLimit: 10,
 	queueLimit: 0,
@@ -14,3 +14,13 @@ const pool = mysql.createPool({
 module.exports = {
 	conn: pool.promise()
 }
+
+
+/*
+
+	host: 'tay.h.filess.io',
+	user: 'solocaps_highwayhot',
+	password: 'e85edbcfa15ffa946f90e243812a388156b02b0f',
+	database: 'solocaps_highwayhot',
+	port: 3307,
+	*/
